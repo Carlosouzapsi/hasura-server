5e6e5744b0c8dc1ab2966ccf57e272c89bba3375
Carlos Otávio de Souza <carlosotavio_souza@apple.com>
2025-04-18 23:40:57 -0300

    starting login service implementation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const UserController = require("../controllers/userController");
-
+const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 const userController = new UserController();
 
 router.post("/users/login", userController.userLogin);
 router.post("/users", userController.createUser);
-router.get("/users", userController.fetchUsers);
+router.get("/users", authMiddleware, userController.fetchUsers);
 
 module.exports = router;
